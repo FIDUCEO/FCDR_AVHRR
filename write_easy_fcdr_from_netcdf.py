7f2c00a468db0787a3d8f7a89e866af7460d4be8
Jonathan Mittaz <j.mittaz@reading.ac.uk>
2017-09-13 20:12:21 +0100

    Fix xarray bug and allows output of FIDUCEO filename

--- write_easy_fcdr_from_netcdf.py
+++ write_easy_fcdr_from_netcdf.py
@@ -1,23 +1,39 @@
+# * Copyright (C) 2017 J.Mittaz University of Reading
+# * This code was developed for the EC project Fidelity and Uncertainty in
+# * Climate Data Records from Earth Observations (FIDUCEO). 
+# * Grant Agreement: 638822
+# *
+# * This program is free software; you can redistribute it and/or modify it
+# * under the terms of the GNU General Public License as published by the Free
+# * Software Foundation; either version 3 of the License, or (at your option)
+# * any later version.
+# * This program is distributed in the hope that it will be useful, but WITHOUT
+# * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# * more details.
+# * 
+# * A copy of the GNU General Public License should have been supplied along
+# * with this program; if not, see http://www.gnu.org/licenses/
+
 from fiduceo.fcdr.writer.fcdr_writer import FCDRWriter
 from fiduceo.fcdr.writer.templates import avhrr
 import sys
 import netCDF4
 import numpy as np
 import datetime
-
+from optparse import OptionParser
 class read_netcdf(object):
 
     def read_data(self,filename):
 
         ncid = netCDF4.Dataset(filename,'r')
 
-        self.lat = ncid.variables['latitude'][:,:]
-        self.lon = ncid.variables['longitude'][:,:]
+        self.noaa_string = ncid.noaa_string
+        self.version = ncid.version
         year = ncid.variables['year'][:]
         month = ncid.variables['month'][:]
         day = ncid.variables['day'][:]
         hours = ncid.variables['hours'][:]
-        self.time = np.zeros(len(year))
         gd = (year > 1900) & (day > 0) & (month > 0) & (hours >= 0)
         yr = year[gd]
         mn = month[gd]
@@ -29,45 +45,45 @@ class read_netcdf(object):
         temp = (temp - minute)*60.
         second = temp.astype(np.int)
         microsec = ((temp-second)*1e6).astype(np.int)
-        date_time = []
+        self.date_time = []
         for i in range(len(hour)):
-            date_time = datetime.datetime(yr[i],mn[i],dy[i],hour[i],minute[i],second[i],microsec[i])
-        self.time[gd] = netCDF4.date2num(date_time,'seconds since 1970-01-01')
-        gd = (year < 1900) | (day < 0) | (month < 0) | (hours < 0)
-        self.time[gd] = float('nan')
-        self.satza = ncid.variables['satza'][:,:]
-        self.solza = ncid.variables['solza'][:,:]
-        self.relaz = ncid.variables['relaz'][:,:]
-        self.ch1 = ncid.variables['ch1'][:,:]
-        self.ch2 = ncid.variables['ch2'][:,:]
+            self.date_time.append(datetime.datetime(yr[i],mn[i],dy[i],hour[i],minute[i],second[i],microsec[i]))
+        self.time = netCDF4.date2num(self.date_time,'seconds since 1970-01-01')
+        self.lat = ncid.variables['latitude'][gd,:]
+        self.lon = ncid.variables['longitude'][gd,:]
+        self.satza = ncid.variables['satza'][gd,:]
+        self.solza = ncid.variables['solza'][gd,:]
+        self.relaz = ncid.variables['relaz'][gd,:]
+        self.ch1 = ncid.variables['ch1'][gd,:]
+        self.ch2 = ncid.variables['ch2'][gd,:]
         try:
-            self.ch3a = ncid.variables['ch3a'][:,:]
+            self.ch3a = ncid.variables['ch3a'][gd,:]
             self.ch3a_there = True
         except:
             self.ch3a_there = False
-        self.ch3b = ncid.variables['ch3b'][:,:]
-        self.ch4 = ncid.variables['ch4'][:,:]
+        self.ch3b = ncid.variables['ch3b'][gd,:]
+        self.ch4 = ncid.variables['ch4'][gd,:]
         try:
-            self.ch5 = ncid.variables['ch5'][:,:]
+            self.ch5 = ncid.variables['ch5'][gd,:]
             self.ch5_there = True
         except:
             self.ch5_there = False
-        self.u_random_ch1 = ncid.variables['ch1_random'][:,:]
-        self.u_random_ch2 = ncid.variables['ch2_random'][:,:]
+        self.u_random_ch1 = ncid.variables['ch1_random'][gd,:]
+        self.u_random_ch2 = ncid.variables['ch2_random'][gd,:]
         if self.ch3a_there:
-            self.u_random_ch3a = ncid.variables['ch3a_random'][:,:]
-        self.u_random_ch3b = ncid.variables['ch3b_random'][:,:]
-        self.u_random_ch4 = ncid.variables['ch4_random'][:,:]
+            self.u_random_ch3a = ncid.variables['ch3a_random'][gd,:]
+        self.u_random_ch3b = ncid.variables['ch3b_random'][gd,:]
+        self.u_random_ch4 = ncid.variables['ch4_random'][gd,:]
         if self.ch5_there:
-            self.u_random_ch5 = ncid.variables['ch5_random'][:,:]
-        self.u_non_random_ch1 = ncid.variables['ch1_non_random'][:,:]
-        self.u_non_random_ch2 = ncid.variables['ch2_non_random'][:,:]
+            self.u_random_ch5 = ncid.variables['ch5_random'][gd,:]
+        self.u_non_random_ch1 = ncid.variables['ch1_non_random'][gd,:]
+        self.u_non_random_ch2 = ncid.variables['ch2_non_random'][gd,:]
         if self.ch3a_there:
-            self.u_non_random_ch3a = ncid.variables['ch3a_non_random'][:,:]
-        self.u_non_random_ch3b = ncid.variables['ch3b_non_random'][:,:]
-        self.u_non_random_ch4 = ncid.variables['ch4_non_random'][:,:]
+            self.u_non_random_ch3a = ncid.variables['ch3a_non_random'][gd,:]
+        self.u_non_random_ch3b = ncid.variables['ch3b_non_random'][gd,:]
+        self.u_non_random_ch4 = ncid.variables['ch4_non_random'][gd,:]
         if self.ch5_there:
-            self.u_non_random_ch5 = ncid.variables['ch5_non_random'][:,:]
+            self.u_non_random_ch5 = ncid.variables['ch5_non_random'][gd,:]
 
         self.nx = self.lat.shape[1]
         self.ny = self.lat.shape[0]
@@ -110,7 +126,7 @@ class read_netcdf(object):
 
         self.read_data(filename)
 
-def main(file_in,file_out):
+def main(file_in,fileout='None'):
     data = read_netcdf(file_in)
 
     writer = FCDRWriter()
@@ -126,7 +142,9 @@ def main(file_in,file_out):
     dataset.attrs["history"] = ""
     dataset.attrs["references"] = "CDF_FCDR_File Spec"
     dataset.attrs["comment"] = "This version is a pre-B one and aims at showing the kind of uncertainties we are aiming to deliver within FIDUCEO. The values are not final ones and should not be used for science purposes."
-    print "File created"
+    dataset.attrs['sensor'] = "AVHRR"
+    dataset.attrs['platform'] = data.noaa_string
+    dataset.attrs['software_version'] = data.version
     # write real data to the variables. All variables initially contain "_FillValue".
     # Not writing to the whole array is completely OK
     dataset.variables["latitude"].data = data.lat
@@ -167,11 +185,25 @@ def main(file_in,file_out):
     #avhrr.AVHRR._create_channel_refl_variable(12835, "Channel 6 Reflectance") 
     # dump it to disk, netcdf4, medium compression
     # writing will fail when the target file already exists
+    if 'None' == fileout:
+        file_out = writer.create_file_name_FCDR_easy('AVHRR',data.noaa_string,\
+                                                         data.date_time[0],\
+                                                         data.date_time[-1],\
+                                                         data.version)
+    else:
+        file_out = fileout
     writer.write(dataset, file_out)
 
 if __name__ == "__main__":
 
-    file_in=sys.argv[1]
-    file_out=sys.argv[2]
-
-    main(file_in,file_out)
+    usage = "usage: %prog [options] arg1 arg2"
+    parser = OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
+    
+    if len(args) != 1 and len(args) != 2:
+        parser.error("incorrect number of arguments")
+    
+    if len(args) == 1:
+        main(args[0])
+    elif len(args) == 2:
+        main(args[0],fileout=args[1])
