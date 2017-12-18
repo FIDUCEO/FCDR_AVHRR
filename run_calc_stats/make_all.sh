183009494a36d84b533b65ea8089fea5865a2e77
Michael Taylor <patternizer@gmail.com>
2017-12-18 04:30:09 +0000

    v0.3pre run_calc_stats

--- run_calc_stats/make_all.sh
+++ run_calc_stats/make_all.sh
@@ -1,3 +1,22 @@
+#!/bin/sh
+
+# * Copyright (C) 2017 M.Taylor University of Reading                                       
+# * This code was developed for the EC project "Fidelity and Uncertainty in                 
+# * Climate Data Records from Earth Observations (FIDUCEO).                                 
+# * Grant Agreement: 638822                                                                 
+# *                                                                                         
+# * This program is free software; you can redistribute it and/or modify it                 
+# * under the terms of the GNU General Public License as published by the Free              
+# * Software Foundation; either version 3 of the License, or (at your option)               
+# * any later version.                                                                      
+# * This program is distributed in the hope that it will be useful, but WITHOUT             
+# * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or                   
+# * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for                
+# * more details.                                                                           
+# *                                                                                         
+# * A copy of the GNU General Public License should have been supplied along                
+# * with this program; if not, see http://www.gnu.org/licenses/                             
+
 rm -f AVHRR*_G_*.dat
 echo ./make_stats.sh AVHRR06_G > make1.sh
 bsub -q short-serial -W24:00 -oo make1.log < make1.sh
